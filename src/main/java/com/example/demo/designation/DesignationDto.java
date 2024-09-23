89209124c96646beb5eca042c0725a68a2270b47
musfiqur24 <musfiqur.utsho@gmail.com>
2024-09-23 14:36:22 +0600

    Get and Post Created

--- src/main/java/com/example/demo/designation/DesignationDto.java
+++ src/main/java/com/example/demo/designation/DesignationDto.java
@@ -1,4 +1,43 @@
 package com.example.demo.designation;
 
 public class DesignationDto {
+
+    private String desigCode;
+    private String desigDesc;
+
+    private String insUser;
+
+    private String updUser;
+
+    public String getDesigCode() {
+        return desigCode;
+    }
+
+    public void setDesigCode(String desigCode) {
+        this.desigCode = desigCode;
+    }
+
+    public String getDesigDesc() {
+        return desigDesc;
+    }
+
+    public void setDesigDesc(String desigDesc) {
+        this.desigDesc = desigDesc;
+    }
+
+    public String getInsUser() {
+        return insUser;
+    }
+
+    public void setInsUser(String insUser) {
+        this.insUser = insUser;
+    }
+
+    public String getUpdUser() {
+        return updUser;
+    }
+
+    public void setUpdUser(String updUser) {
+        this.updUser = updUser;
+    }
 }
